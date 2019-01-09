86d61e8a396b6a40839e519a8f969d57d1dd8af4
diwako <diwako@gmail.com>
2019-01-09 19:10:15 +0100

    Move files into new folder

--- compile.sh
+++ compile.sh
@@ -1,3 +1,1 @@
-cd ..
-armake build -x "compile.sh" -x "gui.hpp" -x "CfgFunctions.hpp" -x "RscCommon.hpp" -x "*_style.hpp" -x "*_controls.hpp" -x "*.tga" -x "*.psd" -x "*.biprivatekey" -k "diwako_dui.biprivatekey" -z -f diwako_dui/ /mnt/d/SteamLibrary/steamapps/common/Arma\ 3/@diwako_dui/addons/diwako_dui.pbo
-cd diwako_dui
+armake build -x "compile.sh" -x "gui.hpp" -x "CfgFunctions.hpp" -x "RscCommon.hpp" -x "*_style.hpp" -x "*_controls.hpp" -x "*.tga" -x "*.psd" -x "*.biprivatekey" -x "*.gitignore" -x "*.md" -x ".git" -k "diwako_dui.biprivatekey" -z -f mod/ /mnt/d/SteamLibrary/steamapps/common/Arma\ 3/@diwako_dui/addons/diwako_dui.pbo

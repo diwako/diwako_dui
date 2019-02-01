cf75c22c74b6bae073f89459e3f1edf8fc7039c0
G3rrus <bot321@gmx.at>
2019-02-01 22:11:02 +0100

    Added Colorblind color schemes and updated authors files

--- mod/color_style.hpp
+++ mod/color_style.hpp
@@ -33,4 +33,92 @@ class diwako_dui_colors
         blue = "#3333FF";
         yellow = "#D9D900";
     };
+
+    class cblind:protanopia
+    {
+        name = "Protanopia";
+
+        white = "#FFFFFF";
+        red = "#828000";
+        green = "#585A31";
+        blue = "#3333CE";
+        yellow = "#D9D934";
+    };
+
+    class cblind:protanomaly
+    {
+        name = "Protanomaly";
+
+        white = "#FFFFFF";
+        red = "#BC4C00";
+        green = "#25881A";
+        blue = "#3333E6";
+        yellow = "#D9D91B";
+    };
+
+    class cblind:deuteranopia
+    {
+        name = "Deuteranopia";
+
+        white = "#FFFFFF";
+        red = "#8FA100";
+        green = "#4C3D3D";
+        blue = "#3333C2";
+        yellow = "#D9D941";
+    };
+
+    class cblind:deuteranomaly
+    {
+        name = "Deuteranomaly";
+
+        white = "#FFFFFF";
+        red = "#B83B00";
+        green = "#29971D";
+        blue = "#3333E2";
+        yellow = "#D9D91F";
+    };
+
+    class cblind:tritanopia
+    {
+        name = "Tritanopia";
+
+        white = "#FFFFFF";
+        red = "#DA0000";
+        green = "#0A5861";
+        blue = "#33A79E";
+        yellow = "#D95E67";
+    };
+
+    class cblind:tritanomaly
+    {
+        name = "Tritanomaly";
+
+        white = "#FFFFFF";
+        red = "#DE0000";
+        green = "#079625";
+        blue = "#3369dA";
+        yellow = "#D99F28";
+    };
+
+    class cblind:achromatopsia
+    {
+        name = "Achromatopsia";
+
+        white = "#FFFFFF";
+        red = "#454545";
+        green = "#787878";
+        blue = "#4A4A4A";
+        yellow = "#C0C0C0";
+    };
+
+    class cblind:achromatomaly
+    {
+        name = "Achromatomaly";
+
+        white = "#FFFFFF";
+        red = "#8E2525";
+        green = "#419E41";
+        blue = "#40409C";
+        yellow = "#CBCB69";
+    };
 };

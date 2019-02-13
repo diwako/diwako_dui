11e7667396200218492c6bbf677f5325cf503fcb
Brett <brett@bmandesigns.com>
2019-02-13 03:36:56 -0600

    spacing and minor fixes

--- addons/radar/CfgEventHandlers.hpp
+++ addons/radar/CfgEventHandlers.hpp
@@ -1,15 +1,15 @@
 class Extended_PreStart_EventHandlers {
-  class ADDON {
-    init = QUOTE(call COMPILE_FILE(XEH_preStart));
-  };
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_preStart));
+    };
 };
 class Extended_PreInit_EventHandlers {
-  class ADDON {
-    init = QUOTE(call COMPILE_FILE(XEH_preInit));
-  };
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_preInit));
+    };
 };
 class Extended_PostInit_EventHandlers {
-  class ADDON {
-    init = QUOTE(call COMPILE_FILE(XEH_postInit));
-  };
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+    };
 };

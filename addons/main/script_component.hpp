3ba1097da17648c303a9224910a99109741f3f77
diwako <diwako@gmail.com>
2019-02-14 22:59:40 +0100

    Disable cba debug

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT main
 #include "\z\diwako_dui\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_MAIN
   #define DEBUG_MODE_FULL

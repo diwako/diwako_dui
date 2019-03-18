376a1119c08182cf167ae2482ac18dd5cab3b8bd
Brett <brett@bmandesigns.com>
2019-03-18 13:31:55 -0600

    use hemtt 0.5.0 build steps

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-hemtt build
-MOVE addons\diwako_dui_main.pbo "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\"
-MOVE addons\diwako_dui_radar.pbo "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\"
-MOVE addons\diwako_dui_indicators.pbo "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\"
-"G:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\DSSignFile\DSSignFile.exe" diwako_dui.biprivatekey "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\diwako_dui_main.pbo"
-"G:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\DSSignFile\DSSignFile.exe" diwako_dui.biprivatekey "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\diwako_dui_radar.pbo"
-"G:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\DSSignFile\DSSignFile.exe" diwako_dui.biprivatekey "D:\SteamLibrary\steamapps\common\Arma 3\@diwako_dui\addons\diwako_dui_indicators.pbo"

a7cc5e6be11c5180bf0a71e455640616ca73d495
diwako <diwako@gmail.com>
2019-07-20 10:04:53 +0200

    Additional pointer styles (wip)

--- addons/radar/CfgPointerStyles.hpp
+++ addons/radar/CfgPointerStyles.hpp
@@ -1,6 +1,18 @@
 class GVAR(pointers) {
-	class standard {
+    class standard {
         name = "$STR_dui_color_standard";
         file = QPATHTO_T(UI\pointing_styles\standard.paa);
+    };
+    // class radar {
+    //     name = "$STR_dui_addon_radar";
+    //     file = QPATHTO_T(UI\pointing_styles\radar.paa);
+    // };
+    class arrow {
+        name = "$STR_dui_radar_pointer_arrow";
+        file = QPATHTO_T(UI\pointing_styles\arrow.paa);
+    };
+    class arrowhead {
+        name = "$STR_dui_radar_pointer_arrowhead";
+        file = QPATHTO_T(UI\pointing_styles\arrowhead.paa);
     };
 };

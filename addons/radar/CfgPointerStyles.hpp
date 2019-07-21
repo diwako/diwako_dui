1b36ad5cf559b570a5cec4f89fb1e516cb44df77
diwako <diwako@gmail.com>
2019-07-21 09:55:20 +0200

    Clean up pointer config

--- addons/radar/CfgPointerStyles.hpp
+++ addons/radar/CfgPointerStyles.hpp
@@ -7,10 +7,6 @@ class GVAR(pointers) {
         name = "$STR_wedge";
         file = QPATHTO_T(UI\pointing_styles\wedge.paa);
     };
-    // class radar {
-    //     name = "$STR_dui_addon_radar";
-    //     file = QPATHTO_T(UI\pointing_styles\radar.paa);
-    // };
     class arrow {
         name = "$STR_dui_radar_pointer_arrow";
         file = QPATHTO_T(UI\pointing_styles\arrow.paa);
